b35e674d90adb727f9221ac4332043cf9a34c9d0
roshini <roshini@ngpwebsmart.com>
2023-12-27 18:10:45 +0530

    displayed detailed show data on the show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,41 +1,55 @@
 import { useParams } from "react-router-dom";
 import { getShowById } from "../api/tvmaze";
 import { useQuery } from "@tanstack/react-query";
+import ShowMainData from "../components/shows/ShowMainData";
+import Details from "../components/shows/Details";
+import Seasons from "../components/shows/Seasons";
+import Cast from "../components/shows/Cast";
 
-// const useShowById = (showId) => {
-//     const [showData , setShowData] = useState(null);
-//     const [showError , setShowError] = useState(null);
-
-//     useEffect(() => {
-//         async function fetchData(){
-//             try{
-//                 const data = await getShowById(showId)
-//                 setShowData(data)
-//            }catch(error){
-//                 setShowError(error)
-//              }
-//         }
-//         fetchData()
-
-//     }, [showId])
-//     return {showData, showError}   
-// }
 
 const Show = () => {
 
     const {showId} = useParams();
-    // const {showData , showError} = useShowById(showId)
     const {data : showData ,error: showError} = useQuery({
         queryKey: ['show' , showId],
         queryFn : ( ) => getShowById(showId),
+        refetchOnWindowFocus : false,
     })
 
     if(showError){
-        <div>we have an error : {showError.message}</div>
+       return <div>we have an error : {showError.message}</div>
     }
 
     if(showData){
-        <div>Got show data{showData.name}</div>
+        return <div>
+            
+            <ShowMainData 
+             image={showData.image}
+             name={showData.name} 
+             rating={showData.rating} 
+             summary={showData.summary} 
+             genres={showData.genres}
+             />
+
+            <div>
+                <h2>Details</h2>
+                <Details
+                 status = {showData.status}
+                 premiered = {showData.premiered}
+                 network = {showData.network}
+                />
+            </div>
+
+            <div>
+                <h1>Seasons</h1>
+                <Seasons seasons={showData._embedded.seasons}/>
+            </div>
+
+            <div>
+                <h1>Cast</h1>
+                <Cast cast={showData._embedded.cast}/>
+            </div>
+        </div>
     }
 
     return <div>Data is loading{showId}</div>
